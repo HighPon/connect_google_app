71ac012e610d56708bc6bb2353ae8899ec536a1c
HighPon <s.shiraki081@gmail.com>
2021-07-28 03:36:53 +0900

    create header

--- view/src/app/app.module.ts
+++ view/src/app/app.module.ts
@@ -7,11 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatIconModule} from '@angular/material/icon';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { LeftBarComponent } from './left-bar/left-bar.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { PrimaryHeaderComponent } from './primary-header/primary-header.component';
+import { NavbarHeaderComponent } from './navbar-header/navbar-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftBarComponent,
+    PrimaryHeaderComponent,
+    NavbarHeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,6 +28,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatIconModule,
     MatDatepickerModule,
     MatFormFieldModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
