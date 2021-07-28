d17dec40d246fc6d4e27a72466049c22a6b407f0
HighPon <s.shiraki081@gmail.com>
2021-07-29 08:59:18 +0900

    Set new content to header

--- view/src/app/app.module.ts
+++ view/src/app/app.module.ts
@@ -16,7 +16,10 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { LeftBarComponent } from './left-bar/left-bar.component';
 import { MatRadioModule } from '@angular/material/radio';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -40,6 +43,9 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     MatRadioModule,
     FormsModule,
     ReactiveFormsModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
